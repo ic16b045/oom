1fb4873b1564c16ea20a9dd807fec0f256455fd0
Magdalena Kurek <ic16b045@technikum-wien.at>
2017-06-19 22:42:02 +0200

    async/await

--- tasks/Task6/Task6/Program.cs
+++ tasks/Task6/Task6/Program.cs
@@ -56,6 +56,7 @@ namespace Task6
 
     class Program
     {
+        //Array mit Filmen(static braucht keine Instanz)
         private static Movie[] filme => new Movie[]
             {
                 new Movie("Test 0", 0),
@@ -73,14 +74,12 @@ namespace Task6
 
             //Filtern - nur die Filme die ein Rating haben - Action filter_filme schreibt nach einer Sleep-Zeit den Namen des Films auf die Console
             subj.Where(filme => filme.Rating > 0).Subscribe(filter_filme => { Thread.Sleep(1000); Console.WriteLine(filter_filme.Name); });
-
+        
             //Filme werden gepusht
             foreach (var film in filme)
             {
                 subj.OnNext(film);
             }
-          
-
           
             //subj. wird entsorgt
             subj.Dispose();
@@ -88,35 +87,81 @@ namespace Task6
 
             /*Anfang Task 6.2*/
     
+            //Listen für tasks und continue.with
             var tasks = new List<Task<string>>();
-            var tasks_cwl = new List<Task<int>>();
+            var tasks_cwl = new List<Task<string>>();
 
 
             foreach (var z in filme)
             {
+                //Starte Task; Warte 2 Sek; gib Namen zurück;
                 var task = Task.Run(() =>
                 {
                     Console.WriteLine($"Neuer Task für Film erstellt. Wird jetzt irgendwas damit getan: {z.Name}");
-                    Task.Delay(TimeSpan.FromSeconds(2.0));
+                    Task.Delay(TimeSpan.FromSeconds(2.0)).Wait();
                     Console.WriteLine($"Irgendwas ist fertig mit {z.Name}");
                     return z.Name;
                 });
+                //Task in Liste hinzufügen
                 tasks.Add(task);
                 Console.WriteLine($"Task {z.Name} hinzugefügt");
             }
          
             foreach (var t in tasks)
             {
-               
+               //gib Name aus, wenn Task fertig
                 tasks_cwl.Add(
-                    t.ContinueWith(cw_fertig => { Console.WriteLine($"result is {cw_fertig.Id}"); return cw_fertig.Id; })
+                    t.ContinueWith(cw_fertig => { Console.WriteLine($"Name is {cw_fertig.Result}"); return cw_fertig.Result; })
                );
             }
-          
+
+           // Console.ReadLine();
+           
             foreach (var t in tasks)
             {
+                //warte bis Task fertig
                 t.Wait();
+            }
+
+            //Ermöglicht das manuelle Beenden während der Task läuft
+            var can_tok_src = new CancellationTokenSource();
+            //Berechneetwas wird gestartet + Möglichkeit zum Beenden
+            var ber_task = Berechneetwas(can_tok_src.Token);
+
+            Console.ReadLine(); 
+            can_tok_src.Cancel(); //Berechneetwas stoppt
+            Console.WriteLine("Berechnung beendet");
+
+            Console.ReadLine();
+        }
+
+        public static Task<bool> Gerade_Zahl(int x, CancellationToken can_tok_src)
+        {
+            return Task.Run(() =>
+            {
+                //Ist Zahl gerade?
+                for (var i = 0; i < x + 1; i++) 
+                {
+                    //wenn man etwas eingibt, wird die Operation beendet
+                    can_tok_src.ThrowIfCancellationRequested();
+                    //keine gerade Zahl
+                    if (x % 2 != 0) return false; 
+                }
+                return true; //ansonsten - gerade Zahl
+            }, can_tok_src);
+        }
+
+       //async muss für await angegeben werden
+        public static async Task Berechneetwas(CancellationToken can_tok_src)
+        {
+            for (var i = 1; i < int.MaxValue; i++)
+            {
+                //wenn man etwas eingibt, wird die Operation beendet
+                can_tok_src.ThrowIfCancellationRequested();
+                //wartet auf Gerade_Zahl, Programm blockiert aber nicht
+                if (await Gerade_Zahl(i, can_tok_src)) Console.WriteLine($"Zahl: {i}");
             }
         }
     }
+    
 }
